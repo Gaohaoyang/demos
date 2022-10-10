006e959cf681567f1ff98421d3a75734b15ec34b
chuanshi <ghy118432@alibaba-inc.com>
2022-10-10 19:23:45 +0800

    Updates

---
+++ static/js/44.9a863c6f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdemos=self.webpackChunkdemos||[]).push([[44],{44:function(e,s,u){u.r(s);u(791);var n=u(184);s.default=function(){return(0,n.jsx)("div",{children:"MultiRedBags"})}}}]);
+//# sourceMappingURL=44.9a863c6f.chunk.js.map
